f27d86a7045d5be9ee83d7efeb60fd1176c7edf7
BiterFire12 <rarijdanil45@gmail.com>
2026-08-15 00:47:54 +0500

    Update MainActivity.kt

--- android/app/src/main/java/kz/shadekz/app/MainActivity.kt
+++ android/app/src/main/java/kz/shadekz/app/MainActivity.kt
@@ -3,15 +3,22 @@ package kz.shadekz.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import kz.shadekz.app.location.AndroidLocationRepository
 import kz.shadekz.app.ui.MainScreen
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var locationRepository: AndroidLocationRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        locationRepository = AndroidLocationRepository(this)
+
         setContent {
-            MainScreen()
+            MainScreen(
+                locationRepository = locationRepository
+            )
         }
     }
 }
